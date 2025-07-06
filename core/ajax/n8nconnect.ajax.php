5d70be26a9f342e5c7f2b76922301d4250fea945
Teeflo <38420614+Teeflo@users.noreply.github.com>
2025-07-06 22:28:46 +0200

    Add full CRUD and workflow management for n8nconnect
    Implemented AJAX endpoints and frontend logic for listing, creating, retrieving, updating, and deleting n8nconnect equipment in Jeedom. Added workflow listing from n8n, improved error handling, and enhanced UI interactions for equipment and workflow management.

--- core/ajax/n8nconnect.ajax.php
+++ core/ajax/n8nconnect.ajax.php
@@ -7,6 +7,8 @@ try {
         throw new Exception(__('401 - Accès non autorisé', __FILE__));
     }
 
+    require_once dirname(__FILE__) . '/../class/n8nconnect.class.php';
+
     ajax::init();
 
     if (init('action') == 'test') {
@@ -38,6 +40,98 @@ try {
         throw new Exception($msg);
     }
 
+    if (init('action') == 'listWorkflows') {
+        try {
+            $data = n8nconnect::callN8n('GET', '/workflows');
+            $result = [];
+            if (isset($data['data'])) {
+                foreach ($data['data'] as $wf) {
+                    $result[] = ['id' => $wf['id'], 'name' => $wf['name']];
+                }
+            }
+            ajax::success($result);
+        } catch (Exception $e) {
+            log::add('n8nconnect', 'error', 'Erreur lors de la récupération des workflows : ' . $e->getMessage());
+            ajax::error($e->getMessage());
+        }
+    }
+
+    // Gestion des actions d'équipement standard de Jeedom
+    if (init('action') == 'add') {
+        $eqLogic = new n8nconnect();
+        $eqLogic->setName(__('Nouveau workflow n8n', __FILE__));
+        $eqLogic->setEqType_name('n8nconnect');
+        $eqLogic->setIsEnable(1);
+        $eqLogic->setIsVisible(1);
+        $eqLogic->save();
+        ajax::success(utils::o2a($eqLogic));
+    }
+    
+    if (init('action') == 'get') {
+        $eqLogic = n8nconnect::byId(init('id'));
+        if (!is_object($eqLogic)) {
+            throw new Exception(__('Équipement introuvable', __FILE__));
+        }
+        ajax::success(utils::o2a($eqLogic));
+    }
+    
+    if (init('action') == 'save') {
+        $eqLogicData = json_decode(init('eqLogic'), true);
+        log::add('n8nconnect', 'debug', 'Données reçues pour sauvegarde : ' . json_encode($eqLogicData));
+        
+        if (!is_array($eqLogicData)) {
+            throw new Exception(__('Données d\'équipement invalides', __FILE__));
+        }
+        
+        // Si l'ID est vide, créer un nouvel équipement
+        if (empty($eqLogicData['id'])) {
+            log::add('n8nconnect', 'debug', 'Création d\'un nouvel équipement');
+            $eqLogic = new n8nconnect();
+        } else {
+            log::add('n8nconnect', 'debug', 'Modification de l\'équipement ID : ' . $eqLogicData['id']);
+            $eqLogic = n8nconnect::byId($eqLogicData['id']);
+            if (!is_object($eqLogic)) {
+                throw new Exception(__('Équipement introuvable', __FILE__));
+            }
+        }
+        
+        utils::a2o($eqLogic, jeedom::fromHumanReadable($eqLogicData));
+        $eqLogic->save();
+        log::add('n8nconnect', 'debug', 'Équipement sauvegardé avec succès, ID : ' . $eqLogic->getId());
+        ajax::success(utils::o2a($eqLogic));
+    }
+    
+    if (init('action') == 'remove') {
+        $eqLogic = n8nconnect::byId(init('id'));
+        if (!is_object($eqLogic)) {
+            throw new Exception(__('Équipement introuvable', __FILE__));
+        }
+        $eqLogic->remove();
+        ajax::success();
+    }
+    
+    if (init('action') == 'getAll') {
+        $eqLogics = n8nconnect::byType('n8nconnect');
+        $result = [];
+        foreach ($eqLogics as $eqLogic) {
+            $result[] = utils::o2a($eqLogic);
+        }
+        ajax::success($result);
+    }
+    
+    if (init('action') == 'getCmd') {
+        $eqLogic = n8nconnect::byId(init('id'));
+        if (!is_object($eqLogic)) {
+            throw new Exception(__('Équipement introuvable', __FILE__));
+        }
+        $cmds = $eqLogic->getCmd();
+        $result = [];
+        foreach ($cmds as $cmd) {
+            $result[] = utils::o2a($cmd);
+        }
+        ajax::success($result);
+    }
+    
     throw new Exception(__('Aucune méthode correspondante à', __FILE__) . ' : ' . init('action'));
 } catch (Exception $e) {
     ajax::error(displayException($e), $e->getCode());
